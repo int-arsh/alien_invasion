5faffcbd7798baafb9a7c76d3fa6b28bb8bdab96
int-arsh <jainaakash303@gmail.com>
2025-12-11 17:55:35 +0530

    Add high score tracking and display; update scoreboard and game stats

--- src/game_stats.py
+++ src/game_stats.py
@@ -4,6 +4,7 @@ class GameStats:
         self.set = ai.set
         self.reset_stats()
         self.game_active = False
+        self.high_score = 0
     
     def reset_stats(self):
         """Initialise stats that can change during the game"""
--- src/main.py
+++ src/main.py
@@ -156,6 +156,7 @@ class AlienInvasion:
             for alien in collisions.values():
                 self.stats.score += self.set.alien_points * len(alien)
             self.sb.prep_score()
+            self.sb.check_high_score()
         
         if not self.aliens:
             self.bullets.empty()
